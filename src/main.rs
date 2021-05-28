12412c7cfc561355bb6bdbca193e5eed432bc0b2
Edward Boland <mr.eboland@gmail.com>
2021-05-28 15:05:07 -0400

    struct basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+
+
+
+    // Structs
+
+    // Rust structs, sometimes called structures, resemble classes in Python, or objects in JS. A struct assembles several values of assorted types together into a single value, so we can deal with them as a unit. Given a struct, you can read and modify its individual components. A struct can have methods associated with it that operate on its components.
+
+    // Rust has three kinds of struct types. Named-field, tuple-like, and unit-like which each differ in how we refer to their components. A named-field struct gives a name to each component, whereas a tuple-like struct identifies them by the order in which they appear. Unit-like structs have no components at all. These are not common, but more useful then you'd think.
+
+
+
+    
+    
 }
